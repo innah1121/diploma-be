887e3c68ffa96c295f0a1570ad4cfd947e6caa91
s.torneo <s.torneo@zerynth.com>
2021-03-15 20:16:14 +0100

    Fix go files structure

---
+++ backend/src/go.mod
@@ -0,0 +1,9 @@
+module backend
+
+go 1.15
+
+require (
+	github.com/cs161-staff/userlib v0.0.0-20210222025138-b2753f6faffd
+	github.com/google/uuid v1.2.0
+	github.com/gorilla/mux v1.8.0
+)
---
+++ backend/src/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"backend/function"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Article - Our struct for all articles
+type Article struct {
+	Id      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var Articles []Article
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/register", signUp).Methods("POST")
+	myRouter.HandleFunc("/login", signIn)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func signUp(w http.ResponseWriter, r *http.Request) {
+	var p Credentials
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Println(p.Username)
+	user, _ := function.InitUser(p.Username, p.Password)
+	fmt.Println("User is getting registered.Username : " + user.Username)
+	fmt.Fprintf(w, "Person: %+v", p)
+}
+
+func signIn(w http.ResponseWriter, r *http.Request) {
+	var p Credentials
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	user, error := function.GetUser(p.Username, p.Password)
+	if error != nil {
+		http.Error(w, error.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Println("Trying to get user with username : " + user.Username)
+	fmt.Fprintf(w, "Person: %+v", p)
+	json.NewEncoder(w).Encode(user.Username)
+}
+
+func createEmployee(w http.ResponseWriter, r *http.Request) {
+	headerContentTtype := r.Header.Get("Content-Type")
+	if headerContentTtype != "application/json" {
+		errorResponse(w, "Content Type is not application/json", http.StatusUnsupportedMediaType)
+		return
+	}
+	var e Credentials
+	var unmarshalErr *json.UnmarshalTypeError
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+	err := decoder.Decode(&e)
+	if err != nil {
+		if errors.As(err, &unmarshalErr) {
+			errorResponse(w, "Bad Request. Wrong Type provided for field "+unmarshalErr.Field, http.StatusBadRequest)
+		} else {
+			errorResponse(w, "Bad Request "+err.Error(), http.StatusBadRequest)
+		}
+		return
+	}
+	errorResponse(w, "Success", http.StatusOK)
+	return
+}
+
+func errorResponse(w http.ResponseWriter, message string, httpStatusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(httpStatusCode)
+	resp := make(map[string]string)
+	resp["message"] = message
+	jsonResp, _ := json.Marshal(resp)
+	w.Write(jsonResp)
+}
+
+func main() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
+
+	handleRequests()
+}
